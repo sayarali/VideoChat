e3b52eafcace2cdb6d3f934535c6c133251d9a56
Ali Sayar <alisayar07@gmail.com>
2023-05-10 23:56:54 +0300

    main activity done

--- app/src/main/java/com/alisayar/videochat/MainActivity.kt
+++ app/src/main/java/com/alisayar/videochat/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.alisayar.videochat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alisayar.videochat.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.usernameButton.setOnClickListener {
+
+            if(binding.username.text.toString() != "") {
+                val intent = Intent(this, CallActivity::class.java)
+                    .putExtra("username", binding.username.text.toString())
+                startActivity(intent)
+            } else {
+                Snackbar.make(binding.root, "Kullanıcı adınızı giriniz!", Snackbar.LENGTH_LONG).show()
+            }
+
+
+
+        }
+
+
+
     }
 }
