7605263d53df99eeab4f784d0ff4fe90c4d14b67
Ali Sayar <alisayar07@gmail.com>
2023-05-11 21:43:16 +0300

    Permission request

--- app/src/main/java/com/alisayar/videochat/MainActivity.kt
+++ app/src/main/java/com/alisayar/videochat/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alisayar.videochat.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import com.permissionx.guolindev.PermissionX
+import com.permissionx.guolindev.callback.RequestCallback
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +17,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.usernameButton.setOnClickListener {
-
             if(binding.username.text.toString() != "") {
-                val intent = Intent(this, CallActivity::class.java)
-                    .putExtra("username", binding.username.text.toString())
-                startActivity(intent)
+
+                PermissionX.init(this)
+                    .permissions(
+                        android.Manifest.permission.RECORD_AUDIO,
+                        android.Manifest.permission.CAMERA
+                    ).request { allGranted, _ , _ ->
+                        if(allGranted){
+                            val intent = Intent(this, CallActivity::class.java)
+                                .putExtra("username", binding.username.text.toString())
+                            startActivity(intent)
+                        } else {
+                            Snackbar.make(binding.root, "Lütfen tüm izinleri veriniz!", Snackbar.LENGTH_LONG).show()
+                        }
+                    }
+
             } else {
                 Snackbar.make(binding.root, "Kullanıcı adınızı giriniz!", Snackbar.LENGTH_LONG).show()
             }
