1f4bfa2d485b38503e85f1b232d2275ae0ccf750
Ali Sayar <alisayar07@gmail.com>
2023-05-11 13:36:59 +0300

    CallActivity

--- app/src/main/java/com/alisayar/videochat/CallActivity.kt
+++ app/src/main/java/com/alisayar/videochat/CallActivity.kt
@@ -2,10 +2,33 @@ package com.alisayar.videochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.alisayar.videochat.databinding.ActivityCallBinding
+import com.alisayar.videochat.models.MessageModel
+import com.alisayar.videochat.util.NewMessageInterface
 
-class CallActivity : AppCompatActivity() {
+class CallActivity : AppCompatActivity(), NewMessageInterface {
+
+    lateinit var binding: ActivityCallBinding
+    private var username: String? = null
+
+    private var socketRepository: SocketRepository? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_call)
+        binding = ActivityCallBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        username = intent.getStringExtra("username")
+        socketRepository = SocketRepository(this)
+        username?.let {
+            socketRepository?.initSocket(it)
+        }
+    }
+
+    override fun onNewMessage(message: MessageModel) {
+
     }
 }
